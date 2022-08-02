8de98bb7253b155672ee29941a9229120fd62526
arcticdeveloper1 <ibrahimyilmaz99999@gmail.com>
2022-08-02 20:01:48 +0300

    [#4] Design of fragment_home.xml

--- app/src/main/java/com/imgy/todo_app/fragments/HomeFragment.kt
+++ app/src/main/java/com/imgy/todo_app/fragments/HomeFragment.kt
@@ -1,15 +1,19 @@
 package com.imgy.todo_app.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.google.android.material.appbar.MaterialToolbar
+import com.imgy.todo_app.R
 import com.imgy.todo_app.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
     private lateinit var binding: FragmentHomeBinding
+    private lateinit var toolbar: MaterialToolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +25,19 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
+        toolbar = binding.toolbar.materialToolbar
+        toolbar.title = "Hi Ibrahim"
+        toolbar.subtitle = "Welcome to your bucket list!"
+        toolbar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.action_profile -> {
+                    Toast.makeText(context, "profile clicked", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
+
+        }
         return binding.root
     }
 
